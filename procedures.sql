5adcd4e98c2e35d5db894b2e60b5bac5dae97068
Edward Lynch-Milner <18222021@studentmail.ul.ie>
2020-09-07 21:32:35 +0100

    Updated procedures.sql as of issues #39 and #40

--- procedures.sql
+++ procedures.sql
@@ -1,6 +1,5 @@
 delimiter //
 
-/*This may not be correct, draw out example tables with data and step through each data following this procedure*/
 CREATE PROCEDURE calculate_grades (
     IN module CHAR(6),
     IN student INTEGER
@@ -16,11 +15,15 @@ CREATE PROCEDURE calculate_grades (
               AND 
               exam IN(SELECT id FROM exams WHERE exams.module = module); 
 
+        IF mark IS NULL THEN
+            SET mark = 0;
+        END IF;
+
         IF (SELECT COUNT(*) FROM module_grades WHERE module_grades.module = module AND module_grades.student = student) = 0 THEN
             INSERT INTO module_grades (module, student, mark) VALUES (module, student, mark);
         ELSE
             UPDATE module_grades
-            SET module_grades.mark = mark
+            SET module_grades.mark = mark, date_recorded = NOW()
             WHERE module_grades.module = module AND module_grades.student = student;
         END IF;
     END;//
